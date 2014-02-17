d11008bdf4e64a7aff2a919425fe340b284bebfd
stefanussoegianto92@gmail.com <stefanussoegianto92@gmail.com@faf9dcfe-e777-f306-0a5f-c3f146653e43>
2014-02-17 18:08:48 +0000

    Update Form Supplier

--- StockApps/StockApps/Program.cs
+++ StockApps/StockApps/Program.cs
@@ -15,7 +15,7 @@ namespace StockApps
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new _administrator());
         }
     }
 }
